79dcbf8c1f0fabd3efda66ab374b965f4d59e686
jiangzhouq <jiangzhouq@gmail.com>
2014-10-31 09:48:40 +0800

    删除除了图书外其他的识别handler

--- src/com/google/zxing/client/android/result/ResultHandlerFactory.java
+++ src/com/google/zxing/client/android/result/ResultHandlerFactory.java
@@ -16,6 +16,8 @@
 
 package com.google.zxing.client.android.result;
 
+import android.util.Log;
+
 import com.google.zxing.Result;
 import com.google.zxing.client.android.CaptureActivity;
 import com.google.zxing.client.result.ParsedResult;
@@ -33,28 +35,11 @@ public final class ResultHandlerFactory {
   public static ResultHandler makeResultHandler(CaptureActivity activity, Result rawResult) {
     ParsedResult result = parseResult(rawResult);
     switch (result.getType()) {
-      case ADDRESSBOOK:
-        return new AddressBookResultHandler(activity, result);
-      case EMAIL_ADDRESS:
-        return new EmailAddressResultHandler(activity, result);
-      case PRODUCT:
-        return new ProductResultHandler(activity, result, rawResult);
-      case URI:
-        return new URIResultHandler(activity, result);
-      case WIFI:
-        return new WifiResultHandler(activity, result);
-      case GEO:
-        return new GeoResultHandler(activity, result);
-      case TEL:
-        return new TelResultHandler(activity, result);
-      case SMS:
-        return new SMSResultHandler(activity, result);
-      case CALENDAR:
-        return new CalendarResultHandler(activity, result);
       case ISBN:
         return new ISBNResultHandler(activity, result, rawResult);
       default:
-        return new TextResultHandler(activity, result, rawResult);
+    	  //do something to tell there is a wrong code.
+        return null;
     }
   }
 
